1ec284eab5236ea8ad816d4a59d45136f420d114
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-14 10:59:31 -0500

    set up callback logic in index.js for fetchMyIp function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// handles main fetch function
+const { fetchMyIp } = require('./iss');
+
+fetchMyIp((error, ip) => {
+  if (error) {
+    console.log("It didn't work.", error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
--- iss.js
+++ iss.js
@@ -4,6 +4,8 @@
  * @returns (via callback):
  *  - an error (if any)
  *  - The IP address if available (null if error)
+ * URL for ipify IPV4 JSON format IP address
+ * @link https://api64.ipify.org?format=json
  */
 
 const fetchMyIp = (callback) => {
