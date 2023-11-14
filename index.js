3c64cefb866d12bff9572900245f69546be2d5ba
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-14 13:33:08 -0500

    move waterfall callbacks from index.js to iss.js inside the nextISSTimesForMyLocation function and finish with callback passing data to index.js

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const { fetchMyIp, fetchCoordsByIp, issFlyoverTimes, nextISSTimesForMyLocation } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIp((error, ip) => {
-//   if (error) {
-//     console.log("IP ERROR:", error.message);
-//     return;
-//   }
-
-//   return fetchCoordsByIp(ip, (error, userLocation) => {
-//     if (error) {
-//       console.log("FETCH COORDINATES ERROR:", error.message);
-//       return;
-//     }
-//     return issFlyoverTimes(userLocation, (error, data) => {
-//       if (error) {
-//         console.log("ISS LOCATION ERROR:", error.message);
-//         return;
-//       }
-//       return data;
-//     });
-//   });
-// });
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     console.log(error);
     return;
   }
-  console.log(passTimes);
+  console.log("passTimes", passTimes);
 });
 
--- iss.js
+++ iss.js
@@ -98,7 +98,26 @@ const issFlyoverTimes = (coordinates, callback) => {
  */
 
 const nextISSTimesForMyLocation = (callback) => {
+  fetchMyIp((error, ip) => {
+    if (error) {
+      console.log("IP ERROR:", error.message);
+      return;
+    }
 
+    return fetchCoordsByIp(ip, (error, userLocation) => {
+      if (error) {
+        console.log("FETCH COORDINATES ERROR:", error.message);
+        return;
+      }
+      return issFlyoverTimes(userLocation, (error, data) => {
+        if (error) {
+          console.log("ISS LOCATION ERROR:", error.message);
+          return;
+        }
+        return callback(null, data);
+      });
+    });
+  });
 };
 
-module.exports = { fetchMyIp, fetchCoordsByIp, issFlyoverTimes, nextISSTimesForMyLocation };
+module.exports = { nextISSTimesForMyLocation };
